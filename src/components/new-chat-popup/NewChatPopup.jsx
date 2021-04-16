adb52cd4261d5c4acf5611aa7effc4790d84ab57
Horhik <horhik@tuta.io>
2021-04-16 19:28:43 +0300

    make Add Chat popup working with core

--- src/components/new-chat-popup/NewChatPopup.jsx
+++ src/components/new-chat-popup/NewChatPopup.jsx
@@ -1,15 +1,19 @@
 import { useContext, useState, useRef } from "preact/hooks"
+import {AppContext} from "../../context"
 const NewChatPopup = () => {
   const textarea = useRef();
   const [userName, updateUserName] = useState("");
+  const ws = useContext(AppContext).self.ws
+  console.log("WS IS HERE", ws);
   const sendForm = () => {
     try{
     let parsed = JSON.parse(textarea.current.value)
     parsed.name = userName;
     console.log(parsed)
-      let inlen = parsed.insert_key.length;
+      let inlen = parsed.insertKey.length;
     if (userName.length < 2) {throw "Please, change username"}
       if (inlen == 99 || inlen == 100 || inlen == 91){
+        ws.send(JSON.stringify({...parsed, type: "addUser", name: userName }))
       }
       else{
         throw "Failed to parse insert_key\n";
@@ -35,7 +39,7 @@ const NewChatPopup = () => {
         />
       </div>
       <textarea ref={textarea} placeholde="somehow">
-        {'{\"insert_key\":\"SSK@RJ~uACMHPfiaOUmwGdh5gBNnDbGeLjr1TVcvBMFcdWM,k5Vd7RwqOUcXgKGTl~cR4cUorKv24M4R~tCBAUp2yZo,AQECAAE\",\"sign_key\":\"foo\"}'}
+        {'{\"insertKey\":\"SSK@RJ~uACMHPfiaOUmwGdh5gBNnDbGeLjr1TVcvBMFcdWM,k5Vd7RwqOUcXgKGTl~cR4cUorKv24M4R~tCBAUp2yZo,AQECAAE\",\"signKey\":\"foo\"}'}
       </textarea>
       <button onClick={sendForm}>Submit</button>
     </section>
